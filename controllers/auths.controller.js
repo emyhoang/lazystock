6540b0971c5011ddadfce5fbdc28813d51a146f6
emyhoang <emyhoang8899@gmail.com>
2019-04-05 20:45:39 -0700

    Add dummy data to login and register controllers

--- controllers/auths.controller.js
+++ controllers/auths.controller.js
@@ -1,9 +1,15 @@
-const login = () => {
-
+const login = (req, res) => {
+  res.status(200)
+  res.json({
+    "message": "User logged in: " + req.body.email
+  })
 }
 
-const register = () => {
-  
+const register = (req, res) => {
+  res.status(200)
+  res.json({
+    "message": "User registered: " + req.body.email
+  })
 }
 
 module.exports = {
