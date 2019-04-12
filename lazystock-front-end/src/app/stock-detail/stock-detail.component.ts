cc39fe44d4cca1e13043c4754d0dec35ba3b013f
emyhoang <emyhoang8899@gmail.com>
2019-04-12 16:14:11 -0700

    show stock details in html and get stock details from backend

--- lazystock-front-end/src/app/stock-detail/stock-detail.component.ts
+++ lazystock-front-end/src/app/stock-detail/stock-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { StockService } from '../stock.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-stock-detail',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-detail.component.css']
 })
 export class StockDetailComponent implements OnInit {
-
-  constructor() { }
+  id: any;
+  details: any;
+  constructor(private auth: AuthService,
+    private stock: StockService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
-  }
+    if (!this.auth.isLoggedIn()) {
+      this.router.navigate(['/login']);
+    };
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get('id');
+    });
+    this.stockDetails(this.id);
+  };
+
+  stockDetails(id: any){
+    this.stock.getStockById(id).subscribe( res => {
+      this.details = res['stock'];
+    }, (err) => {
+      console.log(err);
+    });
+  };
 
 }
