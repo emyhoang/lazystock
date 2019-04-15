1190a037c4530b83ade7ab9e71ab063d2f3489ee
emyhoang <emyhoang8899@gmail.com>
2019-04-15 16:00:29 -0700

    change register design

--- lazystock-front-end/src/app/login/login.component.ts
+++ lazystock-front-end/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
@@ -7,7 +7,6 @@ import { Router } from '@angular/router';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
   loginForm = new FormGroup({
